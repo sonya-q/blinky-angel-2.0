2fa307e6227fc2efeef6cef5110e45d4c4538c45
Sonya <sonyacqu@gmail.com>
2025-11-22 10:12:55 -0500

    add: background.js running correctly

--- background.js
+++ background.js
@@ -1,24 +1,41 @@
-// store settings
-let settings = {
-  enabled: true,
-  mode: 'balanced', // strict, balanced, gentle
-  breakInterval: 20 // minutes, allows for user to change by preference
-}
+// check extension properly installed
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Blinky Angel 2.0 successfully installed!");
+  
+  // set default settings, runs one time
+  chrome.storage.local.set({
+    enabled: true,
+    mode: 'balanced', // strict, balanced, gentle
+    breakInterval: 20 // minutes, allows for user to change by preference
+  });
+});
 
-// timer
 
-let breakTimer = null;
-
-function startBreakTimer() {
-  breakTimer = setInterval(() => {
-    // every twenty minutes
-    sentBreakReminder();
-  }, settings.breakInterval * 60 * 1000);
-}
-
-// listen for messages from popup/content
+// listen for messages from background
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.type === "GET_SETTINGS") {
-    sendResponse(settings);
-  } // to be added to later
-})
+  console.log("Background message received");
+  if (msg.type === "GET_SETTINGS"){
+    // get settings from storage
+    chrome.local.storage.get(['enabled', 'mode', 'breakInterval'], (result) => {
+      console.log("Got settings: ", result);
+      sendResponse(result);
+    });
+    return true; // keeps message channel open for asynch response
+  };
+
+  if (msg.type === "UPDATE_SETTINGS"){
+    // save new settings
+    chrome.storage.local.set(msg.settings, () => {
+      console.log("Updating settings", msg.settings);
+      sendResponse({success: True});
+    })
+    return true;
+  };
+
+  if (msg.type === "BLINK_DETECTED"){
+    console.log("Blink detected! Count: ", msg.count);
+    sendResponse({received: True});
+  };
+});
+
+console.log("Background running")
